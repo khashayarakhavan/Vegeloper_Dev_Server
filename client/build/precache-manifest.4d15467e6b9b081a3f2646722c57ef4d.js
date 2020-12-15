636b148d22e2891152451abfaa4170e945afa11e
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-12-15 00:02:10 -0800

    First step in Production Server :rocket:

---
+++ client/build/precache-manifest.4d15467e6b9b081a3f2646722c57ef4d.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df1db3668d0e965ab55e08878dfd8ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8be4f33f71ed5bcdcb1d",
+    "url": "/static/css/main.433314be.chunk.css"
+  },
+  {
+    "revision": "92013364b8ceaf95e7b3",
+    "url": "/static/js/0.19d26dc6.chunk.js"
+  },
+  {
+    "revision": "df38c46ff26487e22d11",
+    "url": "/static/js/1.817c10bf.chunk.js"
+  },
+  {
+    "revision": "fc245e551f00a4974a0a",
+    "url": "/static/js/10.c1718029.chunk.js"
+  },
+  {
+    "revision": "1db5c2a089bc1722699b",
+    "url": "/static/js/11.44728a44.chunk.js"
+  },
+  {
+    "revision": "be8c09d5e9e7d257c117",
+    "url": "/static/js/12.e0e350eb.chunk.js"
+  },
+  {
+    "revision": "943b2d54fadd3d347c6b",
+    "url": "/static/js/13.362b7a40.chunk.js"
+  },
+  {
+    "revision": "39e329468f7de04aca03",
+    "url": "/static/js/14.2e54323b.chunk.js"
+  },
+  {
+    "revision": "c63127abe48ccdbbf174",
+    "url": "/static/js/4.c354d042.chunk.js"
+  },
+  {
+    "revision": "e2690203f6827acaacbf",
+    "url": "/static/js/5.2ddc04df.chunk.js"
+  },
+  {
+    "revision": "e2c258ccbd0651cf54ae",
+    "url": "/static/js/6.b55fd538.chunk.js"
+  },
+  {
+    "revision": "baab8d07d9487a6b778f",
+    "url": "/static/js/7.a07d96c5.chunk.js"
+  },
+  {
+    "revision": "445c7a99f71c7269446d",
+    "url": "/static/js/8.65a9f710.chunk.js"
+  },
+  {
+    "revision": "421445a9db94b027f7df",
+    "url": "/static/js/9.a3e02486.chunk.js"
+  },
+  {
+    "revision": "8be4f33f71ed5bcdcb1d",
+    "url": "/static/js/main.02de67cf.chunk.js"
+  },
+  {
+    "revision": "5823817bbd11dcfc2540",
+    "url": "/static/js/runtime~main.52ddedb0.js"
+  },
+  {
+    "revision": "a96011404e1db5d4cdbe642987cc3b6d",
+    "url": "/static/media/Vegeloper.a9601140.svg"
+  },
+  {
+    "revision": "94e3728d4e541710a2deaa7b42fbd8a8",
+    "url": "/static/media/WebWeaver-Line.94e3728d.svg"
+  },
+  {
+    "revision": "f250e12628a7b2a12cb9141fa2e5cda1",
+    "url": "/static/media/back.f250e126.jpg"
+  },
+  {
+    "revision": "3574fb442c7fcc0ae147ddb5e0a30757",
+    "url": "/static/media/chevron-thin-right.3574fb44.svg"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "/static/media/heart.7746f5e4.svg"
+  },
+  {
+    "revision": "5120ad222b9080b343187d3c51e851f3",
+    "url": "/static/media/house-1.5120ad22.jpeg"
+  },
+  {
+    "revision": "ed12ff11bf26663331ad01ff18d3ebfb",
+    "url": "/static/media/house-2.ed12ff11.jpeg"
+  },
+  {
+    "revision": "521c065d9e8bc6ff2ec850a3ea58a4bf",
+    "url": "/static/media/house-3.521c065d.jpeg"
+  },
+  {
+    "revision": "83ee1d1e0e83198aa12b8a63af3f8a2b",
+    "url": "/static/media/house-4.83ee1d1e.jpeg"
+  },
+  {
+    "revision": "721effb4a9ef7f3ac5e281cb13cda239",
+    "url": "/static/media/house-5.721effb4.jpeg"
+  },
+  {
+    "revision": "ee021f8ac6ebfadeab35a43e6ef6b306",
+    "url": "/static/media/house-6.ee021f8a.jpeg"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "ce776cff0588d8e8385c75a3c8958f0d",
+    "url": "/static/media/linea-basic-10.ce776cff.svg"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/static/media/linea-basic-10.e6763f00.ttf"
+  },
+  {
+    "revision": "ee16c7b8a84ac078afc2b46923cc6187",
+    "url": "/static/media/logo-bi.ee16c7b8.png"
+  },
+  {
+    "revision": "e5c174a02df208c513cfb932428e84c9",
+    "url": "/static/media/logo-forbes.e5c174a0.png"
+  },
+  {
+    "revision": "a473e3878ca044d42ba86f99b685ba47",
+    "url": "/static/media/logo-techcrunch.a473e387.png"
+  },
+  {
+    "revision": "764685dbee21c14ef7a112168e92b429",
+    "url": "/static/media/logo.764685db.png"
+  },
+  {
+    "revision": "55d92fde6e5669b3d9d75e60be7d28ed",
+    "url": "/static/media/me-dev.55d92fde.svg"
+  },
+  {
+    "revision": "6a6400b177acd3859374523f0030e003",
+    "url": "/static/media/me.6a6400b1.svg"
+  },
+  {
+    "revision": "4107b773b93217f31bb7bf8630d6ef04",
+    "url": "/static/media/sprite-medals.4107b773.svg"
+  },
+  {
+    "revision": "24268b24588bddc750dc3b45bd820459",
+    "url": "/static/media/story-1.24268b24.jpeg"
+  },
+  {
+    "revision": "915f2bd1ebf80f9d775aa9954f294269",
+    "url": "/static/media/story-2.915f2bd1.jpeg"
+  }
+]);
